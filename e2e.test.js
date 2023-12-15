faf3fd85aed3bdfd337b5f1757017c8948e8674a
Ulises Santana <ulisesantana@gmail.com>
2023-12-15 15:23:17 +0000

    fix (test): remove e2e tests from CI

--- e2e.test.js
+++ e2e.test.js
@@ -3,10 +3,8 @@ const { spawn } = require('child_process')
 const fetch = require('node-fetch')
 const assert = require('node:assert')
 
-// Start the server as a child process
-const server = spawn('npm', ['start'])
+const server = spawn('node', ['--env-file=.env', 'index.js'])
 
-// Give the server some time to start
 setTimeout(() => fetch('http://localhost:4567/hfswapi/getPeople/1')
   .then(response => response.json())
   .then(response => {
@@ -18,6 +16,18 @@ setTimeout(() => fetch('http://localhost:4567/hfswapi/getPeople/1')
       name: 'Luke Skywalker'
     })
     console.log('✅ /hfswapi/getPeople/:id from database')
+    return fetch('http://localhost:4567/hfswapi/getPeople/2')
+  })
+  .then(response => response.json())
+  .then(response => {
+    assert.deepEqual(response, {
+      height: 167,
+      homeworld_id: '/planets/1',
+      homeworld_name: 'Tatooine',
+      mass: 75,
+      name: 'C-3PO'
+    })
+    console.log('✅ /hfswapi/getPeople/:id from SWAPI')
     return fetch('http://localhost:4567/hfswapi/getPlanet/1')
   })
   .then(response => response.json())
@@ -27,23 +37,38 @@ setTimeout(() => fetch('http://localhost:4567/hfswapi/getPeople/1')
       name: 'Tatooine'
     })
     console.log('✅ /hfswapi/getPlanet/:id from database')
+    return fetch('http://localhost:4567/hfswapi/getPlanet/42')
+  })
+  .then(response => response.json())
+  .then(response => {
+    assert.deepEqual(response, {
+      gravity: 0.98,
+      name: 'Haruun Kal'
+    })
+    console.log('✅ /hfswapi/getPlanet/:id from SWAPI')
     return fetch('http://localhost:4567/hfswapi/getLogs')
   })
   .then(response => response.json())
   .then(response => {
     assert.equal(Array.isArray(response.data), true)
+    assert.equal(response.data.length, 5)
     assert.equal(response.currentPage, 1)
     assert.equal(response.pageSize, 10)
+    assert.equal(response.pageCount, 1)
     console.log('✅ /hfswapi/getLogs')
-  })
-  .then(() => {
-    server.kill()
-    process.exit(0)
-  })
-  .catch(error => {
-    // If there's an error, log it and exit with failure
+  }).catch(error => {
     console.error('❌ Test failed:', error)
-    server.kill()
-    process.exit(1)
-  }).finally()
-, 500) // Adjust this timeout according to how long your server takes to start
+  }).finally(() => {
+    killServer()
+    process.exit()
+  })
+, 500)
+
+process.on('exit', killServer)
+process.on('SIGINT', killServer)
+process.on('SIGTERM', killServer)
+process.on('uncaughtException', killServer)
+
+function killServer () {
+  server.kill('SIGKILL')
+}
